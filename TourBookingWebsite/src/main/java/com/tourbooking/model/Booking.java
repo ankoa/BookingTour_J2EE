a4570a5b85c8e9adc6237380ed0124e2e20e69d4
quanglinh <116327425+Linh1023@users.noreply.github.com>
2024-11-20 15:38:06 +0700

    fix model booking update dto

--- TourBookingWebsite/src/main/java/com/tourbooking/model/Booking.java
+++ TourBookingWebsite/src/main/java/com/tourbooking/model/Booking.java
@@ -1,7 +1,10 @@
 package com.tourbooking.model;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -37,6 +40,12 @@ public class Booking {
     @Column(name = "time", nullable = false)
     private LocalDateTime time;
 
+    @Column(name="total_discount")
+    private Integer totalDiscount;
+
+    @Column(name="payment_method")
+    private String paymentMethod;
+
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "tour_time_id", nullable = false)
@@ -47,6 +56,8 @@ public class Booking {
     @JoinColumn(name = "customer_id", nullable = false, referencedColumnName = "customer_id")
     private Customer customer;
 
-    @Column(name="total_discount")
-    private Integer totalDiscount;
+
+    @OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Set<BookingDetail> bookingDetails;
 }
